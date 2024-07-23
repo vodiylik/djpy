6bb299e2feedefa5de59a22c40a6d303a7b1426b
Shukrullo Turgunov <shookrullo@gmail.com>
2024-07-23 19:08:08 +0500

    footer copy is changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -118,7 +118,7 @@ function SmallPrint() {
   return (
     <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 sm:flex-row dark:border-white/5">
       <p className="text-xs text-zinc-600 dark:text-zinc-400">
-        &copy; Copyright {new Date().getFullYear()}. All rights reserved.
+        &copy; <a href="https://vodiylik.dev">Vodiylik aka</a> {new Date().getFullYear()}. Barcha huquqlar himoyalangan.
       </p>
       <div className="flex gap-4">
         <SocialLink href="#" icon={XIcon}>
